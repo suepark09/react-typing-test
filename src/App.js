edb29502b5151a0cef654ab191e1d491baa65d09
Sue Park <suepandrice06@gmail.com>
2019-12-16 10:06:21 -0600

    fixed timer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Navigation from './Navbar';
 
-import Input from './Input';
+// import Input from './Input';
 import Header from './Header';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// const initialState = {
+//   text: 'Hello world',
+//   userInput: '',
+//   symbols: 0
+// }
+
 class App extends React.Component {
+
+  // state = initialState;
   
   render() {
   
@@ -14,9 +22,6 @@ class App extends React.Component {
       <div className="App">
         <Navigation />
         <Header />
-        
-
-      <Input />
       
 
 
