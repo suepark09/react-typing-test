b6ebc6d4b3fa5249379fb578ad9bed24add0db78
Sue Park <suepandrice06@gmail.com>
2019-12-17 08:22:19 -0600

    implemented basic functionality

--- src/Header.js
+++ src/Header.js
@@ -9,11 +9,18 @@ class Header extends React.Component {
         super(props);
         this.state = {
             seconds: '60',
+            characters: 0,
             words: 0,
+            accuracy: 0,
             started: false
         }
         this.startCountDown = this.startCountDown.bind(this);
         this.tick = this.tick.bind(this);
+        this.resetTimer = this.resetTimer.bind(this);
+        this.addCPM = this.addCPM.bind(this);
+        this.addWPM = this.addWPM.bind(this);
+        this.addAccuracy = this.addAccuracy.bind(this); 
+        console.log('++++', this.state.characters + 1)
       }    
 
 tick () {
@@ -28,9 +35,17 @@ tick () {
 
             if (seconds === 0) {
                 clearInterval(this.intervalHandle);
-                alert(
-                `you type with a speed of ${this.state.symbols} WPM`
-                )
+
+                if (this.state.words > 50) {
+                    alert(
+                        ` Ura wizard! You type with a speed of ${this.state.words} WPM, ${this.state.characters} CPM, with ${this.state.accuracy}% accuracy!`
+                        )
+                }  if (this.state.words < 10)  {
+                    alert(
+                        ` ura turtle! You type with a speed of ${this.state.words} WPM, ${this.state.characters} CPM, with ${this.state.accuracy}% accuracy!`
+                        )
+                }
+              
             }
 
     }
@@ -39,31 +54,51 @@ tick () {
         clearInterval(this.myInterval)
     }
 
-      startCountDown() {
+    startCountDown() {
             if(!this.state.started) {
                 this.setState({started: true});
                 this.intervalHandle = setInterval(this.tick, 1000);
+                // console.log('wuts seconds', this.state.seconds)
             }          
-        }
+    }
+
+    resetTimer() {
+        this.setState({ seconds: 0, characters: 0, words: 0 });
+     }
+
+    addCPM(count) { //waiting until you complete loop in input.js or it'll fire multiple times and not register the incorrect characters
+        this.setState({ characters: count });
+ 
+    }
+
+    addWPM(count) {
+        this.setState({ words: count });
+    }
+
+    addAccuracy(count) {
+        this.setState({accuracy: count });
+    }
 
     render() {
     
     const { seconds }  = this.state
     const { words } = this.state
+    const { characters } = this.state
+    const { accuracy } = this.state
     // const userInput = props.userInput.split('')
-    console.log('yo is this')
+    // console.logIO
       return  <React.Fragment>
           <Jumbotron fluid className="jumbotron justify-content-center">
             <Container className="count-container">
                 <Row className="char-count justify-content-md-center">
                     < Col xs lg="2">
-                        <h1>{ words }</h1><p className= "count-name">WORDS/MIN</p>
+    <h1>{ words }</h1><p className= "count-name">WORDS/MIN</p>
                     </Col>
                     <Col xs lg="2">
-                        <h1>0</h1><p className= "count-name">CHAR/MIN</p>
+    <h1>{ characters } </h1><p className= "count-name">CHAR/MIN</p>
                     </Col>
                     <Col xs lg="2">
-                        <h1>0</h1><p className= "count-name">% ACCURACY</p>
+    <h1>{ accuracy }</h1><p className= "count-name">% ACCURACY</p>
                     </Col>
                 </Row>
                 <Row>
@@ -74,7 +109,7 @@ tick () {
                 </Row>
             </Container>
         </Jumbotron>
-        <Input countDown={ this.startCountDown } />
+        <Input countDown={ this.startCountDown } resetTimer={ this.resetTimer } characters={ this.addCPM } words={ this.addWPM } accuracy={ this.addAccuracy }/>
       </React.Fragment>
     }
   }
--- src/Input.js
+++ src/Input.js
@@ -6,27 +6,77 @@ import './App.css';
 
 const initialState = {
     text: getText(),
-    userInput: '',
-    symbols: 0
+    userInput: ''
 }
+//===STOP
+// const text = initialState.text.split('')
+// const userInput = initialState.userInput.split('')
 
 class Input extends React.Component {
 
         state = initialState;
-
+      
         onRestart = () => {
             this.setState(initialState)
+            this.props.resetTimer();
         }
-
+  
         //targets each character being typed which would then update the state accordingly
         userInputChange = (e) => {
             const value = e.target.value;
+        
             this.setState({
                 userInput: value
             })
-            this.props.countDown()
-        }
+            this.props.countDown() //Starts Timer
 
+            let characterCount = 0 //being passed as an argument to incrememt value
+            let wordCount = 0
+            let accuracyCount = 0
+
+            //COUNT WORD
+
+            for(let i = 0; i < this.state.text.length; i++) {
+                const word = this.state.text.split(' ')[i]
+
+                if(i < value.length) {
+                    if (word === value.split(' ')[i]) {
+                        wordCount = wordCount + 1;
+                    }
+                }
+            }
+
+            this.props.words(wordCount)
+
+            //COUNT CHARACTERS
+
+            for(let i = 0; i < this.state.text.length; i++) { //looping through 
+                const s = this.state.text[i] //s is the index for every single letter
+                if(i < value.length) { //check if theres actually something to compare
+                    if (s === value[i]) { 
+                    characterCount = characterCount + 1; 
+                      }
+                  } 
+            }
+            this.props.characters(characterCount)
+
+            //COUNT ACCURACY
+
+            for(let i = 0; i < this.state.text.length; i++) {
+                const word = this.state.text.split(' ')[i]
+
+                if(i < value.length) {
+                    if (word === value.split(' ')[i]) {
+                        accuracyCount = ( ((wordCount / word.length) * 100 ) / 100 ).toFixed(2);
+                        console.log('wut is accuracy count', (wordCount / word.length))
+                    }
+                }
+            }
+
+            this.props.accuracy(accuracyCount)
+        
+
+        }
     //!!!! BELOW IS THE BOX SECTION WHERE YOU START TYPING !!!!!
 
     render() {
@@ -34,10 +84,10 @@ class Input extends React.Component {
       return  <React.Fragment>
           <Container className="text-box-container">
         <Row className="search-container">
-        <Text className="text-box" text={this.state.text} userInput={this.state.userInput}/>
+        <Text className="text-box" text={this.state.text} userInput={this.state.userInput} />
         <textarea
             className="form-control mb-3" 
-            placeholder="start typing..."
+            placeholder="Start typing..."
             value={this.state.userInput}
             onChange={this.userInputChange}
         >
