f6274434bcae5e5685d0f4d845bc3f6c8da7d298
Sue Park <suepandrice06@gmail.com>
2019-12-17 09:09:03 -0600

    Updates

---
+++ precache-manifest.567a675f6937ec9dbb12a336b376c4a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ab2d261239ac585f100281d49d1829",
+    "url": "/react-typing-test/index.html"
+  },
+  {
+    "revision": "f55544465e0992e6c9c7",
+    "url": "/react-typing-test/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "901f2a928b3c9faed2c5",
+    "url": "/react-typing-test/static/css/main.e8a25a23.chunk.css"
+  },
+  {
+    "revision": "f55544465e0992e6c9c7",
+    "url": "/react-typing-test/static/js/2.5f6392ae.chunk.js"
+  },
+  {
+    "revision": "901f2a928b3c9faed2c5",
+    "url": "/react-typing-test/static/js/main.8560b7e4.chunk.js"
+  },
+  {
+    "revision": "1692537e25ce73a8929f",
+    "url": "/react-typing-test/static/js/runtime-main.c17fd104.js"
+  }
+]);
